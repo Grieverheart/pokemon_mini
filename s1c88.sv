9030c3dea1dbf48599a480a59e26ff77baf40b0f
Grieverheart <nicktasios@gmail.com>
2022-01-01 18:57:15 +0100

    Added some more pieces towards completing the handling of the reset procedure.

--- s1c88.sv
+++ s1c88.sv
@@ -4,14 +4,16 @@ module s1c88
     input clk,
     input reset,
     input [7:0] data_in,
-    output [7:0] data_out,
     output logic pk,
     output logic pl,
 
+    output logic [7:0] data_out,
     output logic [23:0] address_out,
     output logic [1:0]  bus_status,
     output logic read,
-    output logic write
+    output logic write,
+    output logic sync,
+    output logic iack
 );
 
     // @todo:
@@ -86,86 +88,112 @@ module s1c88
         //.byte_word_field(byte_word_field)
     );
 
-    // @note: I don't know if we can do this. Check metastability?
-    // Perhaps we need to run on negedge clk, but check if pl is 0.
-    always_ff @ (negedge clk)
+    always_ff @ (negedge clk, posedge reset)
     begin
-        pl <= ~pl;
-        if(pl == 1)
-        begin
-            if(reset)
-            begin
-                address_out <= ~0;
-                bus_status <= BUS_COMMAND_IDLE;
-            end
-            else if(reset_counter >= 2)
-            begin
-                address_out <= {9'b0, PC[14:0]};
-                bus_status <= (next_state != STATE_EXECUTE)? BUS_COMMAND_MEM_READ: BUS_COMMAND_MEM_WRITE;
-            end
-        end
-    end
-
-    always_ff @ (posedge clk)
-    begin
-        pk <= ~pk;
         if(reset)
         begin
-            state <= STATE_IDLE;
-            read  <= 0;
-            write <= 0;
+            sync          <= 0;
+            iack          <= 0;
+            address_out   <= ~0;
+            data_out      <= ~0;
+            write         <= 0;
+            pl            <= 0;
+            bus_status    <= BUS_COMMAND_IDLE;
             reset_counter <= 0;
         end
         else if(reset_counter < 2)
         begin
             reset_counter <= reset_counter + 1;
+            if(reset_counter == 1)
+            begin
+                // Output dummy address
+                address_out <= 24'hFACE;
+            end
         end
         else
         begin
-            state <= next_state;
-            case(state)
-                STATE_IDLE:
-                begin
-                    read <= 1;
-                end
+            address_out <= {9'b0, PC[14:0]};
+            //if(iack == 1 && interrupt_type == IRQ_RESET)
+            //begin
+            //    address_out <= 0;
+            //end
+            pl <= ~pl;
 
-                STATE_OPCODE_READ:
+            if(pl == 1)
+            begin
+                sync <= 0;
+                if(state == STATE_OPCODE_READ)
                 begin
-                    opcode <= data_in;
-                    read   <= (next_state == STATE_EXECUTE)? 0: 1;
-                    PC <= PC + 1;
+                    sync <= 1;
                 end
+                bus_status <= (next_state != STATE_EXECUTE)? BUS_COMMAND_MEM_READ: BUS_COMMAND_MEM_WRITE;
+                iack <= 1;
+            end
+        end
+    end
 
-                STATE_OPEXT_READ:
-                begin
-                    opext <= data_in;
-                    read  <= (next_state == STATE_EXECUTE)? 0: 1;
-                    PC <= PC + 1;
-                end
+    always_ff @ (posedge clk, posedge reset)
+    begin
+        if(reset)
+        begin
+            state         <= STATE_IDLE;
+            read          <= 0;
+            pk            <= 0;
+        end
+        else if(reset_counter >= 2)
+        begin
+            pk <= ~pk;
+            if(pk == 1)
+            begin
+                // @note: When do we actually move to the next state?
+                // For read states we probably want to move once every
+                // bus cycle, but that's probably not required for the
+                // execute phase.
+                state <= next_state;
+                case(state)
+                    STATE_IDLE:
+                    begin
+                        read <= 1;
+                    end
 
-                STATE_IMM_LOW_READ:
-                begin
-                    imm_low <= data_in;
-                    read    <= (next_state == STATE_EXECUTE)? 0: 1;
-                    PC <= PC + 1;
-                end
+                    STATE_OPCODE_READ:
+                    begin
+                        opcode <= data_in;
+                        read   <= (next_state == STATE_EXECUTE)? 0: 1;
+                        PC <= PC + 1;
+                    end
 
-                STATE_IMM_HIGH_READ:
-                begin
-                    imm_high <= data_in;
-                    read     <= 0;
-                    PC <= PC + 1;
-                end
+                    STATE_OPEXT_READ:
+                    begin
+                        opext <= data_in;
+                        read  <= (next_state == STATE_EXECUTE)? 0: 1;
+                        PC <= PC + 1;
+                    end
 
-                STATE_EXECUTE:
-                begin
-                    read <= 1;
-                end
+                    STATE_IMM_LOW_READ:
+                    begin
+                        imm_low <= data_in;
+                        read    <= (next_state == STATE_EXECUTE)? 0: 1;
+                        PC <= PC + 1;
+                    end
 
-                default:
-                begin
-                end
-            endcase
+                    STATE_IMM_HIGH_READ:
+                    begin
+                        imm_high <= data_in;
+                        read     <= 0;
+                        PC <= PC + 1;
+                    end
+
+                    STATE_EXECUTE:
+                    begin
+                        read <= 1;
+                    end
+
+                    default:
+                    begin
+                    end
+                endcase
+            end
         end
     end
 
