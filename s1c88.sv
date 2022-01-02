ac9a366ac72354784de5e3dbfdaf8112e712fe9d
Grieverheart <nicktasios@gmail.com>
2022-01-02 19:33:13 +0100

    Implemented handling of reset.

--- s1c88.sv
+++ s1c88.sv
@@ -17,7 +17,6 @@ module s1c88
 );
 
     // @todo:
-    // * Implement interrupt handling? Need to start with the reset interrupt.
     // * Implement decoder?
 
     enum [1:0]
@@ -34,7 +33,17 @@ module s1c88
         STATE_OPEXT_READ    = 3'd2,
         STATE_IMM_LOW_READ  = 3'd3,
         STATE_IMM_HIGH_READ = 3'd4,
-        STATE_EXECUTE       = 3'd5;
+        STATE_EXECUTE       = 3'd5,
+        STATE_EXC_PROCESS   = 3'd6;
+
+    localparam [2:0]
+        EXCEPTION_TYPE_RESET   = 3'd0,
+        EXCEPTION_TYPE_DIVZERO = 3'd1,
+        EXCEPTION_TYPE_NMI     = 3'd2,
+        EXCEPTION_TYPE_IRQ3    = 3'd3,
+        EXCEPTION_TYPE_IRQ2    = 3'd4,
+        EXCEPTION_TYPE_IRQ1    = 3'd5,
+        EXCEPTION_TYPE_NONE    = 3'd6;
 
     reg [2:0] state = STATE_IDLE;
 
@@ -43,7 +52,7 @@ module s1c88
     reg need_imm = 0;
 
     wire [2:0] next_state =
-        (state == STATE_IDLE) ?
+        (state == STATE_IDLE || state == STATE_EXC_PROCESS) ?
             STATE_OPCODE_READ:
 
         (state == STATE_OPCODE_READ) ?
@@ -63,7 +72,7 @@ module s1c88
                            STATE_EXECUTE:
                            STATE_OPCODE_READ;
 
-    reg [15:0] PC;
+    reg [15:0] PC = 16'hFACE;
 
     reg [7:0] opcode;
     reg [7:0] opext;
@@ -71,6 +80,9 @@ module s1c88
     reg [7:0] imm_high;
 
     reg [1:0] reset_counter;
+    reg [2:0] exception = EXCEPTION_TYPE_NONE;
+
+    reg [2:0] exception_process_step;
 
     decode decode_inst
     (
@@ -100,6 +112,7 @@ module s1c88
             pl            <= 0;
             bus_status    <= BUS_COMMAND_IDLE;
             reset_counter <= 0;
+            exception     <= EXCEPTION_TYPE_RESET;
         end
         else if(reset_counter < 2)
         begin
@@ -107,27 +120,41 @@ module s1c88
             if(reset_counter == 1)
             begin
                 // Output dummy address
-                address_out <= 24'hFACE;
+                address_out <= 24'hDEFACE;
             end
         end
         else
         begin
-            address_out <= {9'b0, PC[14:0]};
-            //if(iack == 1 && interrupt_type == IRQ_RESET)
-            //begin
-            //    address_out <= 0;
-            //end
             pl <= ~pl;
 
             if(pl == 1)
             begin
+                address_out <= {9'b0, PC[14:0]};
                 sync <= 0;
+
                 if(state == STATE_OPCODE_READ)
                 begin
+                    if(exception != EXCEPTION_TYPE_NONE)
+                    begin
+                        iack <= 1;
+                    end
                     sync <= 1;
                 end
+                else if(state == STATE_EXC_PROCESS)
+                begin
+                    if(exception_process_step == 1)
+                    begin
+                        address_out <= 0;
+                    end
+                    else if(exception_process_step == 2)
+                    begin
+                        address_out <= 1;
+                        iack        <= 0;
+                        exception   <= EXCEPTION_TYPE_NONE;
+                    end
+                end
+
                 bus_status <= (next_state != STATE_EXECUTE)? BUS_COMMAND_MEM_READ: BUS_COMMAND_MEM_WRITE;
-                iack <= 1;
             end
         end
     end
@@ -143,7 +170,8 @@ module s1c88
         else if(reset_counter >= 2)
         begin
             pk <= ~pk;
-            if(pk == 1)
+            read <= 0;
+            if(pk == 0)
             begin
                 // @note: When do we actually move to the next state?
                 // For read states we probably want to move once every
@@ -153,14 +181,38 @@ module s1c88
                 case(state)
                     STATE_IDLE:
                     begin
-                        read <= 1;
+                    end
+
+                    STATE_EXC_PROCESS:
+                    begin
+                        state <= STATE_EXC_PROCESS;
+                        exception_process_step <= exception_process_step + 1;
+                        if(exception_process_step == 1)
+                        begin
+                            read <= 1;
+                            PC[7:0] <= data_in;
+                        end
+                        else if(exception_process_step == 2)
+                        begin
+                            read <= 1;
+                            PC[15:8] <= data_in;
+                            state <= STATE_OPCODE_READ;
+                        end
                     end
 
                     STATE_OPCODE_READ:
                     begin
-                        opcode <= data_in;
-                        read   <= (next_state == STATE_EXECUTE)? 0: 1;
-                        PC <= PC + 1;
+                        if(exception == EXCEPTION_TYPE_RESET)
+                        begin
+                            state <= STATE_EXC_PROCESS;
+                            exception_process_step <= 0;
+                        end
+                        else
+                        begin
+                            opcode <= data_in;
+                            read   <= (next_state == STATE_EXECUTE)? 0: 1;
+                            PC <= PC + 1;
+                        end
                     end
 
                     STATE_OPEXT_READ:
