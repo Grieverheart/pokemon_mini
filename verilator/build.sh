890e4d4fe2fbb37eb084f38a06c421a601d847b7
Garyuutensei <nicktasios@gmail.com>
2022-01-10 20:21:28 +0100

    Fixed build script include path.

--- verilator/build.sh
+++ verilator/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$VERILATOR_ROOT/bin/verilator -O3 -Wno-fatal -trace --top-module 's1c88' -I.. --cc ../rtl/s1c88.sv --exe s1c88_sim.cpp
+$VERILATOR_ROOT/bin/verilator -O3 -Wno-fatal -trace --top-module 's1c88' -I../rtl --cc ../rtl/s1c88.sv --exe s1c88_sim.cpp
 #verilator -O3 -Wno-fatal -trace --top-module 's1c88' -I.. --cc ../s1c88.sv --exe s1c88_sim.cpp
