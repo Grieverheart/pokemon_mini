0bb79b76cda97fdb87d78fb0955e73185e548e3d
Grieverheart <nicktasios@gmail.com>
2022-06-10 22:36:46 +0200

    More progress implementing IRQ handling.

--- rtl/irq.sv
+++ rtl/irq.sv
@@ -10,13 +10,24 @@ module irq
     input [7:0] bus_data_in,
     output logic [7:0] bus_data_out,
 
-    input irq_copy_complete,
-    input irq_render_done,
-    input irq_32Hz,
-    input irq_8Hz,
-    input irq_2Hz,
-    input irq_1Hz
+    input [4:0] irqs,
 );
+
+reg [3:0] irq_group[0:31] = '{
+    0, 0, 0,                // NMI
+    0, 0,                   // Blitter Group
+    1, 1,                   // Tim3/2
+    2, 2,                   // Tim1/0
+    3, 3,                   // Tim5/4
+    4, 4, 4, 4,             // 256hz clock
+    8, 8, 8, 8,             // IR / Shock sensor
+    5, 5,                   // K1x
+    6, 6, 6, 6, 6, 6, 6, 6, // K0x
+    7, 7, 7                 // Unknown ($1D ~ $1F?)
+};
+
+reg [31:0] reg_irq_priority;
+reg [31:0] reg_irq_active;
 
 reg write_latch;
 always_ff @ (negedge clk)
@@ -36,7 +47,17 @@ always_ff @ (posedge clk)
 begin
     write_latch <= 0;
     if(bus_write) write_latch <= 1;
+
+    for(i = 0; i < 32; ++i)
+    begin
+        if(irqs[i] && reg_irq_active[i] && reg_irq_priority[irq_group[i]] > next_priority)
+        begin
+            next_irq <= i;
+            next_priority <= reg_irq_priority[irq_group[i]];
+        end
+    end
 end
+
 
 //always_comb
 //begin
