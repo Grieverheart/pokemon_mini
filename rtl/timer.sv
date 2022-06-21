2e8fdd91fc5d1efccb1eb42ca1fa1d0d239d8a57
Garyuutensei <nicktasios@gmail.com>
2022-06-21 17:10:35 +0200

    Ready to fire timer IRQs

--- rtl/timer.sv
+++ rtl/timer.sv
@@ -34,56 +34,55 @@ wire osc2_enabled = reg_osc_control[4];
 wire osc_l = reg_osc_control[0];
 wire osc_h = reg_osc_control[1];
 
-localparam [3:0] prescale_osc1[0:7] = '{
-    1, 3, 5, 6, 7, 8, 10, 12
-};
+//localparam [3:0] prescale_osc1[0:7] = '{
+//    1, 3, 5, 6, 7, 8, 10, 12
+//};
+//
+//localparam [2:0] prescale_osc2[0:7] = '{
+//    0, 1, 2, 3, 4, 5, 6, 7
+//};
 
-localparam [2:0] prescale_osc2[0:7] = '{
-    0, 1, 2, 3, 4, 5, 6, 7
-};
-
-// @todo: wrong.
-function tick(input osc, input [2:0] prescale);
+function tick(input osc, input [2:0] pid);
     if(osc == 0)
     begin
-        case(prescale)
+        case(pid)
             3'd0:
-                tick = osc1_count[1:0] == 2'h3;
+                tick = osc1prescaler[0] == 1'h1;
             3'd1:
-                tick = osc1_count[3:0] == 4'hF;
+                tick = osc1prescaler[2:0] == 3'h7;
             3'd2:
-                tick = osc1_count[5:0] == 6'h3F;
+                tick = osc1prescaler[4:0] == 5'h1F;
             3'd3:
-                tick = osc1_count[6:0] == 7'h7F;
+                tick = osc1prescaler[5:0] == 6'h3F;
             3'd4:
-                tick = osc1_count[7:0] == 8'hFF;
+                tick = osc1prescaler[6:0] == 7'h7F;
             3'd5:
-                tick = osc1_count[8:0] == 9'h1FF;
+                tick = osc1prescaler[7:0] == 8'hFF;
             3'd6:
-                tick = osc1_count[10:0] == 11'h7FF;
+                tick = osc1prescaler[9:0] == 10'h3FF;
             3'd7:
-                tick = osc1_count[12:0] == 13'h1FFF;
+                tick = osc1prescaler[11:0] == 12'hFFF;
         endcase
     end
     else
     begin
-        case(prescale)
+        case(pid)
             3'd0:
                 tick = 1;
             3'd1:
-                tick = osc2_count[0] == 1'h1;
+                tick = osc2prescaler[0] == 1'h1;
             3'd2:
-                tick = osc2_count[1:0] == 2'h3;
+                tick = osc2prescaler[1:0] == 2'h3;
             3'd3:
-                tick = osc2_count[2:0] == 3'h7;
+                tick = osc2prescaler[2:0] == 3'h7;
             3'd4:
-                tick = osc2_count[3:0] == 4'hF;
+                tick = osc2prescaler[3:0] == 4'hF;
             3'd5:
-                tick = osc2_count[4:0] == 5'h1F;
+                tick = osc2prescaler[4:0] == 5'h1F;
             3'd6:
-                tick = osc2_count[5:0] == 6'h3F;
+                tick = osc2prescaler[5:0] == 6'h3F;
             3'd7:
-                tick = osc2_count[6:0] == 7'h7F;
+                tick = osc2prescaler[6:0] == 7'h7F;
         endcase
     end
 endfunction
@@ -97,6 +96,12 @@ begin
     end
     else
     begin
+        if(reset_l)
+            reg_control[1] <= 0;
+
+        if(reset_h)
+            reg_control[9] <= 0;
+
         if(write_latch)
         begin
             case(bus_address_in)
@@ -160,30 +165,47 @@ end
 
 reg rt_clk_latch;
 wire rt_clk_edge = (rt_clk & ~rt_clk_latch);
-reg [12:0] osc1_count;
+reg [11:0] osc1prescaler;
 always_ff @ (posedge clk)
 begin
-    osc1_count   <= osc1_count + 1;
+    osc1prescaler   <= osc1prescaler + 1;
     rt_clk_latch <= rt_clk;
+
+    if(reset_l)
+        timer <= reg_preset;
 
     if(mode16)
     begin
         if(enabled_l)
         begin
+            //if(osc1prescaler == 2**prescale_osc1[prescale_l] - 1)
+            //begin
+            //    osc1prescaler <= 0;
             if(tick(osc_l, prescale_l))
             begin
                 if(~osc_l || rt_clk_edge)
                 begin
                     if(timer == 0)
+                    begin
+                        // Fire IRQ
                         timer <= reg_preset;
+                    end
                     else
                         timer <= timer - 1;
+
+                    if(timer <= reg_compare)
+                    begin
+                        // Fire IRQ
+                    end
                 end
             end
         end
     end
     else
     begin
+        if(reset_h)
+            timer[15:8] <= reg_preset[15:8];
+
         if(enabled_l)
         begin
             if(tick(osc_l, prescale_l))
@@ -191,9 +213,17 @@ begin
                 if(~osc_l || rt_clk_edge)
                 begin
                     if(timer == 0)
+                    begin
+                        // Fire IRQ
                         timer[7:0] <= reg_preset[7:0];
+                    end
                     else
                         timer[7:0] <= timer[7:0] - 1;
+
+                    if(timer[7:0] <= reg_compare[7:0])
+                    begin
+                        // Fire IRQ
+                    end
                 end
             end
         end
@@ -205,7 +235,10 @@ begin
                 if(~osc_h || rt_clk_edge)
                 begin
                     if(timer == 0)
+                    begin
+                        // Fire IRQ
                         timer[15:8] <= reg_preset[15:8];
+                    end
                     else
                         timer[15:8] <= timer[15:8] - 1;
                 end
@@ -214,10 +247,10 @@ begin
     end
 end
 
-reg [7:0] osc2_count;
+reg [6:0] osc2prescaler;
 always_ff @ (posedge rt_clk)
 begin
-    osc2_count <= osc2_count + 1;
+    osc2prescaler <= osc2prescaler + 1;
 end
 
 endmodule
