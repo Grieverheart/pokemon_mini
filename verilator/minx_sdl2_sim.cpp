b8205218049fd16ef818f43566d486f7c0ac7aa1
Grieverheart <nicktasios@gmail.com>
2022-07-10 00:52:46 +0200

    Slowly removing register read/write in C++.

--- verilator/minx_sdl2_sim.cpp
+++ verilator/minx_sdl2_sim.cpp
@@ -176,359 +176,25 @@ void write_hardware_register(uint8_t* registers, uint32_t address, uint8_t data)
 {
     switch(address)
     {
-        case 0x00:
-        case 0x01:
-        case 0x02:
-        {
-            PRINTD("Writing hardware register SYS_CTRL%d=", address+1);
-        }
-        break;
-
-        case 0x08:
-        {
-            PRINTD("Writing hardware register SEC_CTRL=");
-        }
-        break;
 
         case 0x10:
         {
             PRINTD("Writing hardware register SYS_BATT=");
-        }
-        break;
-
-        case 0x18:
-        case 0x19:
-        case 0x1A:
-        case 0x1B:
-        case 0x1C:
-        case 0x1D:
-        {
-            PRINTD("Writing hardware register TMR%d_%s=", (address - 0x16) / 2, (address % 2)? "OSC": "SCALE");
-        }
-        break;
-
-        case 0x20:
-        case 0x21:
-        case 0x22:
-        {
-            PRINTD("Writing hardware register IRQ_PRI%d=", address - 0x1F);
-        }
-        break;
-
-        case 0x23:
-        case 0x24:
-        case 0x25:
-        case 0x26:
-        {
-            PRINTD("Writing hardware register IRQ_ENA%d=", address - 0x22);
-        }
-        break;
-
-        case 0x27:
-        case 0x28:
-        case 0x29:
-        case 0x2A:
-        {
-            PRINTD("Writing hardware register IRQ_ACT%d=", address - 0x26);
-            data = registers[address] & ~data;
-        }
-        break;
-
-        case 0x30:
-        case 0x31:
-        case 0x38:
-        case 0x39:
-        case 0x48:
-        case 0x49:
-        {
-            PRINTD("Writing hardware register TMR%d_CTRL_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x32:
-        case 0x33:
-        case 0x3A:
-        case 0x3B:
-        case 0x4A:
-        case 0x4B:
-        {
-            PRINTD("Writing hardware register TMR%d_PRE_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x34:
-        case 0x35:
-        case 0x3C:
-        case 0x3D:
-        {
-            PRINTD("Writing hardware register TMR%d_PVT_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x40:
-        {
-            PRINTD("Writing hardware register TMR256_CTRL=");
-        }
-        break;
-
-        case 0x41:
-        {
-            PRINTD("Writing hardware register TMR256_CNT=");
-        }
-        break;
-
-        case 0x60:
-        {
-            PRINTD("Writing hardware register IO_DIR=");
-        }
-        break;
-
-        case 0x61:
-        {
-            PRINTD("Writing hardware register IO_DATA=");
+            registers[address] = data;
         }
         break;
 
         case 0x70:
         {
             PRINTD("Writing hardware register AUD_CTRL=");
+            registers[address] = data;
         }
         break;
 
         case 0x71:
         {
             PRINTD("Writing hardware register AUD_VOL=");
-        }
-        break;
-
-        case 0x80:
-        {
-            PRINTD("Writing hardware register PRC_MODE=");
-            prc_mode = data & 0x3F;
-        }
-        break;
-
-        case 0x81:
-        {
-            PRINTD("Writing hardware register PRC_RATE=");
-            if((prc_rate & 0xE) != (data & 0xE)) prc_rate = data & 0xF;
-            else prc_rate = (prc_rate & 0xF0) | (data & 0x0F);
-        }
-        break;
-
-        case 0x82:
-        {
-            PRINTD("Writing hardware register PRC_MAP_LO=");
-            prc_map = (prc_map & 0xFFFFF00) | (data & 0xFF);
-        }
-        break;
-
-        case 0x83:
-        {
-            PRINTD("Writing hardware register PRC_MAP_MID=");
-            prc_map = (prc_map & 0xFFF00FF) | ((data & 0xFF) << 8);
-        }
-        break;
-
-        case 0x84:
-        {
-            PRINTD("Writing hardware register PRC_MAP_HI=");
-            prc_map = (prc_map & 0xF00FFFF) | ((data & 0xFF) << 16);
-        }
-        break;
-
-        case 0x85:
-        {
-            PRINTD("Writing hardware register PRC_SCROLL_Y=");
-        }
-        break;
-
-        case 0x86:
-        {
-            PRINTD("Writing hardware register PRC_SCROLL_X=");
-        }
-        break;
-
-        case 0x87:
-        {
-            PRINTD("Writing hardware register PRC_SPR_LO=");
-        }
-        break;
-
-        case 0x88:
-        {
-            PRINTD("Writing hardware register PRC_SPR_MID=");
-        }
-        break;
-
-        case 0x89:
-        {
-            PRINTD("Writing hardware register PRC_SPR_HI=");
-        }
-        break;
-
-        case 0xFE:
-        {
-            switch(data) {
-                case 0x00: case 0x01: case 0x02: case 0x03: case 0x04: case 0x05: case 0x06: case 0x07:
-                case 0x08: case 0x09: case 0x0A: case 0x0B: case 0x0C: case 0x0D: case 0x0E: case 0x0F:
-                    PRINTD("LCD_CTRL: Set column low.\n");
-                    break;
-                case 0x10: case 0x11: case 0x12: case 0x13: case 0x14: case 0x15: case 0x16: case 0x17:
-                case 0x18: case 0x19: case 0x1A: case 0x1B: case 0x1C: case 0x1D: case 0x1E: case 0x1F:
-                    PRINTD("LCD_CTRL: Set column high.\n");
-                    break;
-                case 0x20: case 0x21: case 0x22: case 0x23: case 0x24: case 0x25: case 0x26: case 0x27:
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0x28: case 0x29: case 0x2A: case 0x2B: case 0x2C: case 0x2D: case 0x2E: case 0x2F:
-                    // Modify LCD voltage? (2F Default)
-                    // 0x28 = Blank
-                    // 0x29 = Blank
-                    // 0x2A = Blue screen then blank
-                    // 0x2B = Blank
-                    // 0x2C = Blank
-                    // 0x2D = Blank
-                    // 0x2E = Blue screen (overpower?)
-                    // 0x2F = Normal
-                    // User shouldn't mess with this ones as may damage the LCD
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0x30: case 0x31: case 0x32: case 0x33: case 0x34: case 0x35: case 0x36: case 0x37:
-                case 0x38: case 0x39: case 0x3A: case 0x3B: case 0x3C: case 0x3D: case 0x3E: case 0x3F:
-                    // Do nothing?
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47:
-                case 0x48: case 0x49: case 0x4A: case 0x4B: case 0x4C: case 0x4D: case 0x4E: case 0x4F:
-                case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57:
-                case 0x58: case 0x59: case 0x5A: case 0x5B: case 0x5C: case 0x5D: case 0x5E: case 0x5F:
-                case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67:
-                case 0x68: case 0x69: case 0x6A: case 0x6B: case 0x6C: case 0x6D: case 0x6E: case 0x6F:
-                case 0x70: case 0x71: case 0x72: case 0x73: case 0x74: case 0x75: case 0x76: case 0x77:
-                case 0x78: case 0x79: case 0x7A: case 0x7B: case 0x7C: case 0x7D: case 0x7E: case 0x7F:
-                    // Set starting LCD scanline (cause warp around)
-                    PRINTD("LCD_CTRL: Display start line\n");
-                    break;
-                case 0x80:
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0x81:
-                    PRINTD("LCD_CTRL: Set contrast\n");
-                    break;
-                case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87:
-                case 0x88: case 0x89: case 0x8A: case 0x8B: case 0x8C: case 0x8D: case 0x8E: case 0x8F:
-                case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97:
-                case 0x98: case 0x99: case 0x9A: case 0x9B: case 0x9C: case 0x9D: case 0x9E: case 0x9F:
-                    // Do nothing?
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0xA0:
-                    // Segment Driver Direction Select: Normal
-                    PRINTD("LCD_CTRL: Segment driver direction normal\n");
-                    break;
-                case 0xA1:
-                    // Segment Driver Direction Select: Reverse
-                    PRINTD("LCD_CTRL: Segment driver direction reverse\n");
-                    break;
-                case 0xA2:
-                    // Max Contrast: Disable
-                    PRINTD("LCD_CTRL: Normal voltage bias\n");
-                    break;
-                case 0xA3:
-                    // Max Contrast: Enable
-                    PRINTD("LCD_CTRL: Darker voltage bias\n");
-                    break;
-                case 0xA4:
-                    // Set All Pixels: Disable
-                    PRINTD("LCD_CTRL: Set all pixels disable\n");
-                    break;
-                case 0xA5:
-                    // Set All Pixels: Enable
-                    PRINTD("LCD_CTRL: Set all pixels enable\n");
-                    break;
-                case 0xA6:
-                    // Invert All Pixels: Disable
-                    PRINTD("LCD_CTRL: Invert all pixels disable\n");
-                    break;
-                case 0xA7:
-                    // Invert All Pixels: Enable
-                    PRINTD("LCD_CTRL: Invert all pixels enable\n");
-                    break;
-                case 0xA8: case 0xA9: case 0xAA: case 0xAB:
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0xAC: case 0xAD:
-                    // User shouldn't mess with this ones as may damage the LCD
-                    PRINTD("LCD_CTRL: Damage\n");
-                    break;
-                case 0xAE:
-                    // Display Off
-                    PRINTD("LCD_CTRL: Display off\n");
-                    break;
-                case 0xAF:
-                    // Display On
-                    PRINTD("LCD_CTRL: Display on\n");
-                    break;
-                case 0xB0: case 0xB1: case 0xB2: case 0xB3: case 0xB4: case 0xB5: case 0xB6: case 0xB7:
-                case 0xB8:
-                    // Set page (0-8, each page is 8px high)
-                    PRINTD("LCD_CTRL: Set page\n");
-                    break;
-                case 0xB9: case 0xBA: case 0xBB: case 0xBC: case 0xBD: case 0xBE: case 0xBF:
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0xC0: case 0xC1: case 0xC2: case 0xC3: case 0xC4: case 0xC5: case 0xC6: case 0xC7:
-                    // Display rows from top to bottom as 0 to 63
-                    PRINTD("LCD_CTRL: Scan direction normal\n");
-                    break;
-                case 0xC8: case 0xC9: case 0xCA: case 0xCB: case 0xCC: case 0xCD: case 0xCE: case 0xCF:
-                    // Display rows from top to bottom as 63 to 0
-                    PRINTD("LCD_CTRL: Scan direction mirrored\n");
-                    break;
-                case 0xD0: case 0xD1: case 0xD2: case 0xD3: case 0xD4: case 0xD5: case 0xD6: case 0xD7:
-                case 0xD8: case 0xD9: case 0xDA: case 0xDB: case 0xDC: case 0xDD: case 0xDE: case 0xDF:
-                    // Do nothing?
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0xE0:
-                    // Start "Read Modify Write"
-                    break;
-                case 0xE2:
-                    // Reset
-                    PRINTD("LCD_CTRL: Reset display\n");
-                    break;
-                case 0xE3:
-                    // No operation
-                    PRINTD("LCD_CTRL: ???\n");
-                    break;
-                case 0xEE:
-                    // End "Read Modify Write"
-                    PRINTD("LCD_CTRL: Read modify write\n");
-                    break;
-                case 0xE1: case 0xE4: case 0xE5: case 0xE6: case 0xE7:
-                case 0xE8: case 0xE9: case 0xEA: case 0xEB: case 0xEC: case 0xED: case 0xEF:
-                    // User shouldn't mess with this ones as may damage the LCD
-                    PRINTD("LCD_CTRL: Damage\n");
-                    break;
-                case 0xF0: case 0xF1: case 0xF2: case 0xF3: case 0xF4: case 0xF5: case 0xF6: case 0xF7:
-                    // 0xF1 and 0xF5 freeze LCD and cause malfunction (need to power off the device to restore)
-                    // User shouldn't mess with this ones as may damage the LCD
-                    PRINTD("LCD_CTRL: Damage\n");
-                    break;
-                case 0xF8: case 0xF9: case 0xFA: case 0xFB: case 0xFC: case 0xFD: case 0xFE: case 0xFF:
-                    // Contrast voltage control, FC = Default
-                    // User shouldn't mess with this ones as may damage the LCD
-                    PRINTD("LCD_CTRL: Damage\n");
-                    break;
-            }
-            PRINTD("Writing hardware register LCD_CTRL=");
-        }
-        break;
-
-        case 0xFF:
-        {
-            PRINTD("Writing hardware register LCD_DATA=");
+            registers[address] = data;
         }
         break;
 
@@ -543,6 +209,7 @@ void write_hardware_register(uint8_t* registers, uint32_t address, uint8_t data)
         case 0x62:
         {
             PRINTD("Writing hardware register Unknown=");
+            registers[address] = data;
         }
         break;
 
@@ -552,240 +219,29 @@ void write_hardware_register(uint8_t* registers, uint32_t address, uint8_t data)
             return;
         }
     }
-    registers[address] = data;
     PRINTD("0x%x\n", data);
 }
 
 uint8_t read_hardware_register(const uint8_t* registers, uint32_t address)
 {
-    uint8_t data = registers[address];
     switch(address)
     {
-        case 0x0:
-        case 0x1:
-        case 0x2:
-        {
-            PRINTD("Reading hardware register SYS_CTRL%d=", address+1);
-        }
-        break;
-
-        case 0x8:
-        {
-            PRINTD("Reading hardware register SEC_CTRL=");
-        }
-        break;
-
-        case 0x9:
-        {
-            PRINTD("Reading hardware register SEC_CNT_LO=");
-        }
-        break;
-
-        case 0xA:
-        {
-            PRINTD("Reading hardware register SEC_CNT_MID=");
-        }
-        break;
-
-        case 0xB:
-        {
-            PRINTD("Reading hardware register SEC_CNT_MID=");
-        }
-        break;
-
-        case 0x10:
-        {
-            PRINTD("Reading hardware register SYS_BATT=");
-        }
-        break;
 
         case 0x52:
         {
-            PRINTD("Reading hardware register KEY_PAD=");
-        }
-        break;
-
-        case 0x53:
-        {
-            PRINTD("Reading hardware register CART_BUS=");
-        }
-        break;
-
-        case 0x18:
-        case 0x19:
-        case 0x1A:
-        case 0x1B:
-        case 0x1C:
-        case 0x1D:
-        {
-            PRINTD("Writing hardware register TMR%d_%s=", (address - 0x16) / 2, (address % 2)? "OSC": "SCALE");
-        }
-        break;
-
-        case 0x20:
-        case 0x21:
-        case 0x22:
-        {
-            PRINTD("Reading hardware register IRQ_PRI%d=", address - 0x1F);
-        }
-        break;
-
-        case 0x23:
-        case 0x24:
-        case 0x25:
-        case 0x26:
-        {
-            PRINTD("Reading hardware register IRQ_ENA%d=", address - 0x22);
-        }
-        break;
-
-        case 0x27:
-        case 0x28:
-        case 0x29:
-        case 0x2A:
-        {
-            PRINTD("Reading hardware register IRQ_ACT%d=", address - 0x26);
-        }
-        break;
-
-        case 0x30:
-        case 0x31:
-        case 0x38:
-        case 0x39:
-        case 0x48:
-        case 0x49:
-        {
-            PRINTD("Reading hardware register TMR%d_CTRL_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x32:
-        case 0x33:
-        case 0x3A:
-        case 0x3B:
-        case 0x4A:
-        case 0x4B:
-        {
-            PRINTD("Reading hardware register TMR%d_PRE_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x34:
-        case 0x35:
-        case 0x3C:
-        case 0x3D:
-        {
-            PRINTD("Reading hardware register TMR%d_PVT_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-        }
-        break;
-
-        case 0x36:
-        case 0x37:
-        case 0x3E:
-        case 0x3F:
-        case 0x4E:
-        case 0x4F:
-        {
-            PRINTD("Reading hardware register TMR%d_CNT_%s=", (address > 0x40)? 3: (address - 0x28) / 8, address % 2? "H": "L");
-            PRINTE("** Reading hardware register 0x%x which is a timer register and is not implemented! **\n", address);
-        }
-        break;
-
-        case 0x40:
-        {
-            PRINTD("Reading hardware register TMR256_CTRL=");
-        }
-        break;
-
-        case 0x41:
-        {
-            PRINTD("Reading hardware register TMR256_CNT=");
-        }
-        break;
-
-        case 0x60:
-        {
-            PRINTD("Reading hardware register IO_DIR=");
-        }
-        break;
-
-        case 0x61:
-        {
-            PRINTD("Reading hardware register IO_DATA=");
+            return registers[address];
         }
         break;
 
         case 0x70:
         {
-            PRINTD("Reading hardware register AUD_CTRL=");
+            return registers[address];
         }
         break;
 
         case 0x71:
         {
-            PRINTD("Reading hardware register AUD_VOL=");
-        }
-        break;
-
-        case 0x80:
-        {
-            PRINTD("Reading hardware register PRC_MODE=");
-            data = prc_mode;
-        }
-        break;
-
-        case 0x81:
-        {
-            PRINTD("Reading hardware register PRC_RATE=");
-            data = prc_rate;
-        }
-        break;
-
-        case 0x82:
-        {
-            PRINTD("Reading hardware register PRC_MAP_LO=");
-        }
-        break;
-
-        case 0x83:
-        {
-            PRINTD("Reading hardware register PRC_MAP_MID=");
-        }
-        break;
-
-        case 0x84:
-        {
-            PRINTD("Reading hardware register PRC_MAP_HI=");
-        }
-        break;
-
-        case 0x85:
-        {
-            PRINTD("Reading hardware register PRC_SCROLL_Y=");
-        }
-        break;
-
-        case 0x86:
-        {
-            PRINTD("Reading hardware register PRC_SCROLL_X=");
-        }
-        break;
-
-        case 0x87:
-        {
-            PRINTD("Reading hardware register PRC_SPR_LO=");
-        }
-        break;
-
-        case 0x88:
-        {
-            PRINTD("Reading hardware register PRC_SPR_MID=");
-        }
-        break;
-
-        case 0x89:
-        {
-            PRINTD("Reading hardware register PRC_SPR_HI=");
+            return registers[address];
         }
         break;
 
@@ -799,18 +255,17 @@ uint8_t read_hardware_register(const uint8_t* registers, uint32_t address)
         case 0x55:
         case 0x62:
         {
-            PRINTD("Reading hardware register Unknown=");
+            return registers[address];
         }
         break;
 
         default:
         {
-            PRINTD("Reading hardware register 0x%x=", address);
+            return 0;
         }
         break;
     }
-    PRINTD("0x%x\n", data);
-    return data;
+    return 0;
 }
 
 struct SimData
